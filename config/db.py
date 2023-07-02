5708838276c2d5b09d85c2a723e83e59f49c0937
arcmdle <arcmdle0@gmail.com>
2023-07-02 18:43:54 -0500

    Beu

--- config/db.py
+++ config/db.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient
 
+MONGODB_URI = 'mongodb://admin:admin@localhost:27017/?authSource=admin&readPreference=primary&ssl=false&directConnection=true'
 
-conn = MongoClient('mongodb://admin:admin@localhost:27017/?authSource=admin&readPreference=primary&ssl=false&directConnection=true')
+# This can be an environment variable.
+conn = MongoClient(MONGODB_URI)
